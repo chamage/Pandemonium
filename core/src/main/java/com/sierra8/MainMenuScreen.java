1072e695306cf661e06eadc19fe9430d653692c7
Piotr <piotrskocz03@interia.pl>
2025-03-28 00:13:53 +0100

    Refactor rendering logic into reusable ScreenRenderer class.
    Centralized screen rendering logic in the new `ScreenRenderer` class through the `UiScreenRender` method. Updated `OptionsScreen`, `MainMenuScreen`, and `PlayerDeathScreen` to use this method, reducing code duplication and improving maintainability.

--- core/src/main/java/com/sierra8/MainMenuScreen.java
+++ core/src/main/java/com/sierra8/MainMenuScreen.java
@@ -3,7 +3,6 @@ package com.sierra8;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -95,15 +94,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
-
-        stage.act(Math.min(delta, 1 / 30f));
-        stage.draw();
+        ScreenRenderer.UiScreenRender(delta, batch, background, stage);
     }
 
     @Override
---
+++ core/src/main/java/com/sierra8/ScreenRenderer.java
@@ -0,0 +1,21 @@
+package com.sierra8;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class ScreenRenderer {
+    static void UiScreenRender(float delta, SpriteBatch batch, Texture background, Stage stage) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+
+        stage.act(Math.min(delta, 1 / 30f));
+        stage.draw();
+    }
+}
